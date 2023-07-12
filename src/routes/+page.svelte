48724583ac33c964374be907395c8639bb91bc98
kolown <kolown@gmail.com>
2023-07-12 13:28:17 +0800

    added files for deployment

---
+++ src/routes/+page.svelte
@@ -0,0 +1,53 @@
+<script>
+    import { onMount } from 'svelte';
+    
+    onMount(() => {
+      document.body.style.backgroundColor = '#ffffff';
+    });
+  </script>
+  
+  <main class="bg-white">
+    <div class="max-w-4xl mx-auto p-8">
+      <div class="text-center mb-8">
+        <!-- <h1 class="text-3xl font-bold">KoloWn</h1> -->
+
+        <img src="https://kolown.net/server2/kolowntext.svg" alt="Banner" class="mb-4 w-2/5">
+      </div>
+      
+   
+      <div class="flex flex-col items-left font-serif font-bold text-4xl">
+
+        <a href="/objects" class="text-blue-500 px-4 py-2 m-2 inline-block hover:bg-blue-100 underline">
+          <span class="list-disc ml-2">&#8226;</span> Objects
+        </a>
+       
+       
+        <a href="/404" class="text-blue-500 px-4 py-2 m-2 inline-block hover:bg-blue-100 underline">
+          <span class="list-disc ml-2">&#8226;</span> NetArt
+        </a>
+
+        <a href="/404" class="text-blue-500 px-4 py-2 m-2 inline-block hover:bg-blue-100 underline">
+            <span class="list-disc ml-2">&#8226;</span> Public
+          </a>
+
+
+      
+
+        <!-- <a href="/404" class="text-blue-500 px-4 py-2 m-2 inline-block hover:bg-blue-100 underline">
+            <span class="list-disc ml-2">&#8226;</span> Sketchbook
+          </a>
+      </div> -->
+
+
+
+
+
+
+    </div>
+  </main>
+  
+  <style>
+  
+
+  </style>
+  
