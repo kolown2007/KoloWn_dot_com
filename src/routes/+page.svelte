f805094ed0c65daff694f837ec23db316ebff1ba
kolown <kolown@gmail.com>
2023-10-12 09:52:14 +0800

    updated temporary homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,44 +1,16 @@
 <script lang ="ts">
-  import Objects from "$lib/Circle.svelte"
+  import Objects from "$lib/Objects.svelte"
     
 </script>
 
-<svelte:head>
-  <title>KoloWn</title>
-  <meta name="description" content="KoloWn,
-   Philippine Street Art, Net Art, Ateneo Art Awards,
-    Thirtheen Artists Awards, Contemporary Art, Philippine Art, Meme Art " />
-</svelte:head>
   
   <main class="bg-white">
 
-<!-- header -->
-
-    <div class="max-w-4xl mx-auto p-8">
-      <div class="text-center mb-8">
-        <!-- <h1 class="text-3xl font-bold">KoloWn</h1> -->
-
-        <img src="https://kolown.net/server2/kolowntext.svg" alt="Banner" class="mb-4 w-2/5">
-      </div>
-      
 
 
-      <div class="flex flex-col items-left font-serif font-bold text-4xl">
-
-
-<div>
-  <h1 class="text-blue-600 text-2xl">2023</h1>
-  <ul class=" pl-5 dark:text-gray-400 text-gray-500">
-    <li><a href="/objects/signals" rel="noreferrer" target="_blank"  class="text-blue-600 text-2xl ">Colors of the Wind</a></li>
-    <li><a href="/objects/signals" rel="noreferrer" target="_blank"  class="text-blue-600 text-2xl ">Proof of Work</a></li>
-    <li><a href="/objects/signals" rel="noreferrer" target="_blank"  class="text-blue-600 text-2xl ">Signals</a></li>
-
-   
-
-  </ul>
   
 <Objects/>
-</div>
+
 
 
   </main>
