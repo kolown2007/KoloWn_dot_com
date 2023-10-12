a5192fc8c12a23aed69c4ba9066a3c127f888daf
kolown <kolown@gmail.com>
2023-10-12 09:52:58 +0800

    updated layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,15 @@
 <script>
     import "../app.css";
   </script>
+
+
+
+<svelte:head>
+  <title>KoloWn</title>
+  <meta name="description" content="KoloWn,
+   Philippine Street Art, Net Art, Ateneo Art Awards,
+    Thirtheen Artists Awards, Contemporary Art, Philippine Art, Meme Art " />
+</svelte:head>
   
   <slot />
 
