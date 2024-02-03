f7944ec90d00b6e49bc5e2e6ec8ae0a15449cd46
kolown <kolown@gmail.com>
2024-02-03 12:02:41 +0800

    updated home

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,38 +1,62 @@
 <script lang ="ts">
-  //import Objects from "$lib/Objects.svelte"
+import { AccordionItem, Accordion } from 'flowbite-svelte';
     
 </script>
 
-  
-  <main class="bg-white px-10 py-10">
 
-<div class="text-slate-300" >
+<main class=" px-10 py-10 font-mono bg-blue-700">
+<div class="text-blue-900" >
 
 
   <div>
     <p class=" text-xl"> Ongoing Projects</p>
     <ul>
-      <li><span class ="text-3xl text-slate-600">TradeWinds</span> (2021-Present)</li>
-      <li><span class ="text-3xl text-slate-600">Public Works</span>(2007-Present)</li>
-      <li><span class ="text-3xl text-slate-600">Parallel</span>(2018-Present)</li>
-      <li><span class ="text-3xl text-slate-600">Philippine Collagian</span>(2019-Present)</li>
+      <li><span class ="text-3xl text-slate-200">Public Interventions</span>(2007-Present)</li>
+      <li><span class ="text-3xl text-slate-200">Internet Art</span>(2019-Present)</li>
+      <li><span class ="text-3xl text-slate-200 ">TradeWinds</span> (2021-Present)</li>
+      <li><span class ="text-3xl text-slate-200">Parallel</span>(2018-Present)</li>
+      <li><span class ="text-3xl text-slate-200">Colors of the Wind</span>(2021-Present)</li>
+     
     </ul>
     
     </div>
 
 &nbsp;
 &nbsp;
+
+
 <div>
-<p class=" text-xl"> solo shows</p>
+<p class=" text-xl"> completed projects</p>
 <ul>
-  <li><span class ="text-3xl text-slate-600"> 404</span> (2022,Drawing Room Gallery,PH)</li>
-  <li><span class ="text-3xl text-slate-600">In Real Life</span>(2021,Drawing Room Gallery,PH)</li>
-  <li><span class ="text-3xl text-slate-600">Golden Sigbin</span>(2019,Phyllis Palmer Gallery,AU)</li>
-  <li><span class ="text-3xl text-slate-600">Pay/View</span>(2019,VOV,PH)</li>
-  <li><span class ="text-3xl text-slate-600">Low Pressured Areas</span>(2017,Cultural Center PH)</li>
+  <li><span class ="text-3xl text-slate-200"> For Content Only</span> (2024)</li>
+  <li><span class ="text-3xl text-slate-200"> Proof-of-Work</span> (2022-2023,PH-USA)</li>
+  <li><span class ="text-3xl text-slate-200">P1sonet2022</span>(2022,CCP,PH)</li>
+  <li><span class ="text-3xl text-slate-200">Bituon Project</span>(2019,Cebu-Bali)</li>
 </ul>
 
 </div>
+
+&nbsp;
+&nbsp;
+<div>
+<p class=" text-xl"> solo shows</p>
+<ul>
+  <li><span class ="text-3xl text-slate-200"> 404</span> (2022,Drawing Room Gallery,PH)</li>
+  <li><span class ="text-3xl text-slate-200">In Real Life</span>(2021,Drawing Room Gallery,PH)</li>
+  <li><span class ="text-3xl text-slate-200">Golden Sigbin</span>(2019,Phyllis Palmer Gallery,AU)</li>
+  <li><span class ="text-3xl text-slate-200">Pay/View</span>(2019,VOV,PH)</li>
+  <li><span class ="text-3xl text-slate-200">Low Pressured Areas</span>(2017,Cultural Center PH)</li>
+</ul>
+
+</div>
+&nbsp;
+&nbsp;
+
+<div>
+  <p class=" text-xl text-slate-200"> About</p>
+  
+</div>
+
 
 
 
