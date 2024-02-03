ffeba2445addef98e034731d4b95db0e93ca5dc7
kolown <kolown@gmail.com>
2024-02-03 10:59:29 +0800

    new homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,38 @@
 <script lang ="ts">
-  import Objects from "$lib/Objects.svelte"
+  //import Objects from "$lib/Objects.svelte"
     
 </script>
 
   
-  <main class="bg-white">
+  <main class="bg-white px-10 py-10">
+
+<div class="text-slate-300" >
 
 
+  <div>
+    <p class=" text-xl"> Ongoing Projects</p>
+    <ul>
+      <li><span class ="text-3xl text-slate-600">TradeWinds</span> (2021-Present)</li>
+      <li><span class ="text-3xl text-slate-600">Public Works</span>(2007-Present)</li>
+      <li><span class ="text-3xl text-slate-600">Parallel</span>(2018-Present)</li>
+      <li><span class ="text-3xl text-slate-600">Philippine Collagian</span>(2019-Present)</li>
+    </ul>
+    
+    </div>
 
-  
-<Objects/>
+&nbsp;
+&nbsp;
+<div>
+<p class=" text-xl"> solo shows</p>
+<ul>
+  <li><span class ="text-3xl text-slate-600"> 404</span> (2022,Drawing Room Gallery,PH)</li>
+  <li><span class ="text-3xl text-slate-600">In Real Life</span>(2021,Drawing Room Gallery,PH)</li>
+  <li><span class ="text-3xl text-slate-600">Golden Sigbin</span>(2019,Phyllis Palmer Gallery,AU)</li>
+  <li><span class ="text-3xl text-slate-600">Pay/View</span>(2019,VOV,PH)</li>
+  <li><span class ="text-3xl text-slate-600">Low Pressured Areas</span>(2017,Cultural Center PH)</li>
+</ul>
+
+</div>
 
 
 
