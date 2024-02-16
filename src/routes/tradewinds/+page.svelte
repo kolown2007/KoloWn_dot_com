e374a575ea52038431e415b158c22119a7d80299
kolown <kolown@gmail.com>
2024-02-16 10:36:31 +0800

    fixed some layout errors on traidwinds

--- src/routes/tradewinds/+page.svelte
+++ src/routes/tradewinds/+page.svelte
@@ -4,7 +4,7 @@
 
 </script>
 
-<main class =" py-10 font-mono">
+<main class =" py-10 font-mono place-self-center">
     <h2  class ="text-3xl px-10" >TradeWinds Series</h2>
 
     <div class=" py-10">
@@ -50,24 +50,23 @@
 &nbsp;
 <div class ="py-10 bg-slate-800" id="tradewinds4">
     <h1 class ="px-5 text-slate-300 text-xl ">TradeWinds4</h1>
-    <div class ="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4 p-8" >
+    <div class ="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-2 gap-4 p-8" >
 
-    <div>  <Img src="https://kolown.net/server3/wp-content/uploads/2024/02/Image-e1707980920792.png" size="max-w-lg" alignment="mx-auto" alt="TradeWinds" caption="TradeWinds4 poster with collaborators" /> </div>
+    <div>  <Img src="https://kolown.net/server3/wp-content/uploads/2024/02/Image-e1707980920792.png" size="max-w-full " alignment="mx-auto" alt="TradeWinds" caption="TradeWinds4 poster with collaborators" /> </div>
     <div> <Video src="https://kolown.net/server3/wp-content/uploads/2024/02/tw.mp4" controls class="w-96" trackSrc="flowbite.mp4" /></div>
-    <div>  <Img src="https://kolown.net/server3/wp-content/uploads/2024/02/5838A00A-A633-4813-9EA7-7F973389FC59-518-0000000AC904564F-scaled.jpg" size="max-w-lg" alignment="mx-auto" alt="TradeWinds" caption="installation view" /> </div>
-    <div>  <Img src="https://kolown.net/server3/wp-content/uploads/2024/02/EF3532B1-7CA2-4CE6-B7A1-5869943216EC-518-0000000AD331A119-scaled.jpeg" size="max-w-lg" alignment="mx-auto" alt="TradeWinds" caption="installation view" /> </div>
+    <div>  <Img src="https://kolown.net/server3/wp-content/uploads/2024/02/5838A00A-A633-4813-9EA7-7F973389FC59-518-0000000AC904564F-scaled.jpg" size="max-w-full" alignment="mx-auto" alt="TradeWinds" caption="installation view" /> </div>
+    <div>  <Img src="https://kolown.net/server3/wp-content/uploads/2024/02/EF3532B1-7CA2-4CE6-B7A1-5869943216EC-518-0000000AD331A119-scaled.jpeg" size="max-w-full" alignment="mx-auto" alt="TradeWinds" caption="installation view" /> </div>
  
 </div>
 
 
 <div class ="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 p-8" >
-
-    <div> <Img src="https://kolown.net/server3/wp-content/uploads/2024/02/3.png" size="max-w-sm" alignment="mx-auto" alt="TradeWinds" caption="audience submissions" /></div>
-    <div> <Img src="https://kolown.net/server3/wp-content/uploads/2024/02/2.png" size="max-w-sm" alignment="mx-auto" alt="TradeWinds" caption="audience submissions" /></div>
-    <div> <Img src="https://kolown.net/server3/wp-content/uploads/2024/02/9.png" size="max-w-sm" alignment="mx-auto" alt="TradeWinds" caption="audience submissions" /></div>
-    <div> <Img src="https://kolown.net/server3/wp-content/uploads/2024/02/1.png" size="max-w-sm" alignment="mx-auto" alt="TradeWinds" caption="audience submissions" /></div>
-    <div> <Img src="https://kolown.net/server3/wp-content/uploads/2024/02/8.png" size="max-w-sm" alignment="mx-auto" alt="TradeWinds" caption="audience submissions" /></div>
-    <div> <Img src="https://kolown.net/server3/wp-content/uploads/2024/02/10.png" size="max-w-sm" alignment="mx-auto" alt="TradeWinds" caption="audience submissions" /></div>
+    <div> <Img src="https://kolown.net/server3/wp-content/uploads/2024/02/3.png" size="max-w-full" alignment="mx-auto" alt="TradeWinds" caption="audience submissions" /></div>
+    <div> <Img src="https://kolown.net/server3/wp-content/uploads/2024/02/2.png" size="max-w-full" alignment="mx-auto" alt="TradeWinds" caption="audience submissions" /></div>
+    <div> <Img src="https://kolown.net/server3/wp-content/uploads/2024/02/9.png" size="max-w-full" alignment="mx-auto" alt="TradeWinds" caption="audience submissions" /></div>
+    <div> <Img src="https://kolown.net/server3/wp-content/uploads/2024/02/1.png" size="max-w-full" alignment="mx-auto" alt="TradeWinds" caption="audience submissions" /></div>
+    <div> <Img src="https://kolown.net/server3/wp-content/uploads/2024/02/8.png" size="max-w-full" alignment="mx-auto" alt="TradeWinds" caption="audience submissions" /></div>
+    <div> <Img src="https://kolown.net/server3/wp-content/uploads/2024/02/10.png" size="max-w-full" alignment="mx-auto" alt="TradeWinds" caption="audience submissions" /></div>
 </div>
 </div>
 
@@ -76,8 +75,8 @@
 &nbsp;
 <div class ="py-10 bg-red-800" id="tradewinds3">
     <h1 class ="px-5 text-xl ">TradeWinds: Pier Uno</h1>
-    <div class ="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4 p-8" >
-    <div><Img src="https://kolown.net/server3/wp-content/uploads/2024/02/pieruno.jpg" size="max-w-lg" alignment="mx-auto" alt="TradeWinds" caption="TradeWinds: Pier Uno, 2022, Cebu" /></div>
+    <div class ="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-2 gap-4 p-8" >
+    <div><Img src="https://kolown.net/server3/wp-content/uploads/2024/02/pieruno.jpg" size="max-w-full" alignment="mx-auto" alt="TradeWinds" caption="TradeWinds: Pier Uno, 2022, Cebu" /></div>
     <div> <Video src="https://kolown.net/server3/wp-content/uploads/2024/02/Untitled-video-Made-with-Clipchamp.mp4" controls class="w-96" trackSrc="flowbite.mp4" /></div>
    <div class ="text-yellow-400"> <p> TradeWinds:PierUno</p>
     <p>2022 Visayas Art Fair Special Project </p>
@@ -92,24 +91,27 @@
 
 <div class ="py-10 bg-slate-800" id="tradewinds2">
     <h1 class ="px-5 text-slate-400 text-xl ">TradeWinds: Meteor Garden</h1>
-    <div class ="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4 p-8" >
+    <div class ="grid grid-cols-1 md:grid-cols-1 lg:grid-cols-2 gap-4 p-8" >
     
-<div><Img src="https://dywhz70l8hywb.cloudfront.net/wp-content/uploads/2022/08/10000-Flowers-DAC-01-1.png" size="max-w-lg" alignment="mx-auto" alt="TradeWinds" caption="TradeWinds: Meteor Garden, 2022, Taiwan" /> </div>
+<div><Img src="https://dywhz70l8hywb.cloudfront.net/wp-content/uploads/2022/08/10000-Flowers-DAC-01-1.png" size="max-w-full" alignment="mx-auto" alt="TradeWinds" caption="TradeWinds: Meteor Garden, 2022, Taiwan" /> </div>
 <div class ="text-sm text-slate-400"> 
     <p>Trade Winds_Meteor Garden</p>
-    <p>2022</p>
+    <p>10,000 Flowers Groupshow, 2022</p>
+    &nbsp;
     <p>Projected digital interactive collage installation </p>
   
-    part of exhibition 10000 Flowers, curated by: Gloria Demichelis ,Lo Lok Yin , Yee Chung Kee, Yuji de Torres
+    <p>part of exhibition 10000 Flowers, curated by: Gloria Demichelis ,Lo Lok Yin , Yee Chung Kee, Yuji de Torres</p>
     &nbsp;
-    <p>https://dac.taipei/en/project/10000-flowers/</p>
+    <p><a class="underline" href="https://dac.taipei/en/project/10000-flowers/" >Exhibition Web Archive</a></p>
+    &nbsp;
+    &nbsp;
     <p> 
     TradeWinds_Meteor Garden is the second iteration of the Trade_Winds series. KoloWn’s Trade_Winds explores poetically the effect of exchanges of culture, products and ideas in the globalized world. The title is taken from the Taiwanes Drama Series and one of the first famous “Asianovela ” that was brought to the Philippines in 2003. TradeWinds_Meteor Garden features a generative collage that resembles an ambiguous landscape-painting-inspired composition. The elements will be taken from various sources; from past historical paintings, contemporary internet to pop culture and crowdsourced images from both Taiwan and the Philippines that respond to the participants’ interpretation of “freedom”.
     The installation setup will provide a QR code that would redirect to a website that has a button. If the audience will press the button, it will trigger the elements to appear randomly within the composition.
     The composition that will be generated could be a rough sketch of the contemporary globalized world. Is it harmonious or is it full of contradictions? Is it heaven or is it hell? Do our actions have an impact on the bigger picture?</p>
 </div>
-<div><Img src="https://kolown.net/server3/wp-content/uploads/2024/02/meteorgarden1-1536x1101.jpg" size="max-w-lg" alignment="mx-auto" alt="TradeWinds" caption="TradeWinds: Meteor Garden, 2022, Taiwan" /> </div>
-<div><Img src="https://kolown.net/server3/wp-content/uploads/2024/02/meteorgarden2-scaled.jpg" size="max-w-lg" alignment="mx-auto" alt="TradeWinds" caption="TradeWinds: Meteor Garden, 2022, Taiwan" /> </div>
+<div><Img src="https://kolown.net/server3/wp-content/uploads/2024/02/meteorgarden1-1536x1101.jpg" size="max-w-full" alignment="mx-auto" alt="TradeWinds" caption="TradeWinds: Meteor Garden, 2022, Taiwan" /> </div>
+<div><Img src="https://kolown.net/server3/wp-content/uploads/2024/02/meteorgarden2-scaled.jpg" size="max-w-full" alignment="mx-auto" alt="TradeWinds" caption="TradeWinds: Meteor Garden, 2022, Taiwan" /> </div>
 
 </div>
 
@@ -122,26 +124,32 @@
 <div class ="py-10 bg-red-800" id="tradewinds">
     <h1 class ="px-5 text-xl ">TradeWinds</h1>
 
-    <div class ="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-4 p-8" >
+    <div class ="grid grid-cols-1 md:grid-cols-1 md:justify-center lg:grid-cols-2 gap-4 p-8 mx-auto" >
         <div>
-            <Img src="https://aggp.ca/wp-content/uploads/2021/05/V6_Final-Tuloy-Poster-01-267x400.jpg" size="max-w-xl" alignment="mx-auto" alt="TradeWinds" caption="exhibition poster"  />
+            <Img src="https://aggp.ca/wp-content/uploads/2021/05/V6_Final-Tuloy-Poster-01-267x400.jpg" size="max-w-full" alignment="mx-auto" alt="TradeWinds" caption="exhibition poster"  />
         </div>
-        <div><iframe width="560" height="315" src="https://www.youtube.com/embed/sckBiSCGBIo?si=U9W9iqiEMNj5rdNo&amp;clip=UgkxU4Lg5RYV_6INYU3C9vrhikiK3G7-_iX7&amp;clipt=EPCDOBjQ2Ds" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe></div>
-        <div> <Img src="https://kolown.net/server3/wp-content/uploads/2024/02/MG_2599-scaled.jpg" size="max-w-xl" alignment="mx-auto" alt="TradeWinds" caption="installation view"  /> </div>
+
         <div class ="text-sm text-yellow-400">
-            <p>TradeWinds</p>
-            <p>Tuloy, Tawid, 2021</p>
-            <p>https://aggp.ca/exhibitions/tuloy-tawid/</p>
+            <p class ="font-bold">TradeWinds</p>
+            <p>Tuloy Tawid Groupshow, 2021</p>
+            &nbsp;
+            <p><a class ="underline" href="https://aggp.ca/exhibitions/tuloy-tawid">Exhibition Web Archive</a></p>
+      
             &nbsp;
             <p>crowdsourced trash images from Canada</p>
+            &nbsp;
             <p>TradeWinds (taken from the wind that powers European Colonial expansion) is a browser-based work that can be accessed using smartphones, laptops or anything with a web browser.</p>      
             <p>In response to the current lockdowns imposed to curb the global pandemic, TradeWinds is accessible, as well as flexible in its presentation. For this exhibition, it has been adapted for indoor viewing, but can be modified for different environments.</p>
             
            <p> This work features images of trash crowdsourced from people residing in Canada. Randomly appearing on the screen, the garbage is powerful in its multitude and relentless demand to be seen. Interestingly, the Canadian contributions present a global representation of trash. This is a powerful reminder that such goods cross oceans throughout its lifecycle from raw material to material waste.</p>
         </div>
+
+        <div><iframe class ="w-full aspect-video"  src="https://www.youtube.com/embed/sckBiSCGBIo?si=U9W9iqiEMNj5rdNo&amp;clip=UgkxU4Lg5RYV_6INYU3C9vrhikiK3G7-_iX7&amp;clipt=EPCDOBjQ2Ds" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe></div>
+        <div> <Img src="https://kolown.net/server3/wp-content/uploads/2024/02/MG_2599-scaled.jpg" size="max-w-full" alignment="mx-auto" alt="TradeWinds" caption="installation view"  /> </div>
+        
    
-    <div> <Img src="https://kolown.net/server3/wp-content/uploads/2024/02/Aug-3_4-scaled.jpg" size="max-w-xl" alignment="mx-auto" alt="TradeWinds" caption="installation view"  /> </div>
-    <div> <Img src="https://kolown.net/server3/wp-content/uploads/2024/02/MG_2604-scaled.jpg" size="max-w-xl" alignment="mx-auto" alt="TradeWinds" caption="installation view"  /> </div>
+    <div> <Img src="https://kolown.net/server3/wp-content/uploads/2024/02/Aug-3_4-scaled.jpg" size="max-w-full" alignment="mx-auto" alt="TradeWinds" caption="installation view"  /> </div>
+    <div> <Img src="https://kolown.net/server3/wp-content/uploads/2024/02/MG_2604-scaled.jpg" size="max-w-full" alignment="mx-auto" alt="TradeWinds" caption="installation view"  /> </div>
     
 </div>
 
