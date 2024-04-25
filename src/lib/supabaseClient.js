d8794fd5bb6114228d59c7edb83187d142017e98
KoloWn <kolown@gmail.com>
2024-04-25 14:30:54 +0800

    updates

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,7 +1,5 @@
 import { SUPABASE_URL, SUPABASE_ANON_KEY } from "$env/static/private";
 import { createClient } from "@supabase/supabase-js";
 
-
-
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
         
