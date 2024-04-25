999ddd5fb21622bb66ec30cc44b76aeb11da0fa9
Workers & Artisans Studio <workers.and.artisans@gmail.com>
2024-04-25 14:09:34 +0800

    updates

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,5 +1,7 @@
 import { supabase } from "$lib/supabaseClient";
 
+import { createClient } from "@supabase/supabase-js";
+
 
 export async function load() {
     const { data } = await supabase.from("exhibitions").select("*").order("year", { ascending: false });;
