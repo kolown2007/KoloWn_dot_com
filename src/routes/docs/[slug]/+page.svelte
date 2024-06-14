fe2db7d5eb67762c03d0925c6d6d234e0b63972e
KoloWn <kolown@gmail.com>
2024-06-14 16:52:52 +0800

    feat: Add dynamic slug support to project page route

--- src/routes/docs/[slug]/+page.svelte
+++ src/routes/docs/[slug]/+page.svelte
@@ -9,6 +9,6 @@
 <main class="mx-auto px-4 py-5 max-w-2xl text-white text-lg leading-relaxed ">
 
 
-	<p > {@html data.project.content}</p> 
+	<p > {@html data.doc.content}</p> 
 
 </main> 
--- src/routes/docs/[slug]/+page.ts
+++ src/routes/docs/[slug]/+page.ts
@@ -1,20 +1,22 @@
 import type { PageLoad } from './$types';
 import { GraphQLClient, gql } from 'graphql-request';
 import { PUBLIC_WPGQL} from "$env/static/public";
+import { error } from '@sveltejs/kit';
 
 const client = new GraphQLClient(PUBLIC_WPGQL);
 
 let doc:any;
 
-export const load:PageLoad = (async () => {
+export const load:PageLoad = (async ({ params }) => {
+    const { slug } = params;
 
-    const query = gql`
-    query NewQuery {
-        doc(id: "cG9zdDoyMjU=") {
-            title
-            content(format: RENDERED)
-        }
-    }
+   const query = gql`
+query NewQuery {
+  doc(idType: SLUG, id:"${slug}") {
+    title
+    content(format: RENDERED)
+  }
+}
 `;
 
 const response: { doc: any } = await client.request(query);
@@ -25,8 +27,12 @@ return {
         title: `${doc.title} `,
         content: ` ${doc.content} `,
     },
+
+    
 };
 
 
-  
+
 }) 
+
+error(404, 'Not found');
