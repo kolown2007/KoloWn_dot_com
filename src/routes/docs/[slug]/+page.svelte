a481e80edf1dee43d4cbc25786f7817ccef6501e
KoloWn <kolown@gmail.com>
2024-06-14 17:32:44 +0800

    feat: Update page titles in docs and projects routes

--- src/routes/docs/[slug]/+page.svelte
+++ src/routes/docs/[slug]/+page.svelte
@@ -5,6 +5,9 @@
    export let data: PageData;
 </script>
 
+<svelte:head>
+    <title>KoloWn : Docs:  {data.doc.title}</title>
+</svelte:head>
 
 <main class="mx-auto px-4 py-5 max-w-2xl text-white text-lg leading-relaxed ">
 
--- src/routes/projects/[slug]/+page.svelte
+++ src/routes/projects/[slug]/+page.svelte
@@ -5,6 +5,9 @@
    export let data: PageData;
 </script>
 
+<svelte:head>
+    <title>KoloWn : Project:  {data.project.title}</title>
+</svelte:head>
 
 <main class="mx-auto px-4 py-5 max-w-2xl text-white text-lg leading-relaxed ">
 
