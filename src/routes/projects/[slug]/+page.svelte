522c3c97c9fc9cfc83584ff1ebeaf155e5f4e8eb
KoloWn <kolown@gmail.com>
2024-06-14 16:17:07 +0800

    feat: Update project page to use dynamic slug in route

---
+++ src/routes/projects/[slug]/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    import type { PageData } from './$types';
+    import { page } from '$app/stores';
+    
+   export let data: PageData;
+</script>
+
+
+<main class="mx-auto px-4 py-5 max-w-2xl text-white text-lg leading-relaxed ">
+
+
+	<p > {@html data.project.content}</p> 
+
+</main> 
--- src/routes/projects/[slug]/+page.ts
+++ src/routes/projects/[slug]/+page.ts
@@ -1,18 +1,20 @@
 import type { PageLoad } from './$types';
 import { GraphQLClient, gql } from 'graphql-request';
 import { PUBLIC_WPGQL} from "$env/static/public";
+import { error } from '@sveltejs/kit';
 
 const client = new GraphQLClient(PUBLIC_WPGQL);
 
 let project:any;
 
-export const load:PageLoad = (async () => {
+export const load:PageLoad = (async ({ params }) => {
+    const { slug } = params;
 
-    const query = gql`
-   query NewQuery {
-  projects(id: "cG9zdDoxMzU=") {
-    content(format: RENDERED)
+   const query = gql`
+query NewQuery {
+  projects(idType: SLUG, id:"${slug}") {
     title
+    content(format: RENDERED)
   }
 }
 `;
@@ -25,8 +27,12 @@ return {
         title: `${project.title} `,
         content: ` ${project.content} `,
     },
+
+    
 };
 
 
-  
+
 }) 
+
+error(404, 'Not found');
