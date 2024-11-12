4b9608485fc1a0e68bc8d750b815bca70d4db120
KoloWn <kolown@gmail.com>
2024-11-12 21:35:58 +0800

    Improve GhostWriter story display layout and text styling

--- src/routes/text/ghostwriter/[id]/+page.svelte
+++ src/routes/text/ghostwriter/[id]/+page.svelte
@@ -4,13 +4,15 @@
     export let data: PageData;
 </script>
 
-<main >
+<main>
+    <div class="mx-auto max-w-[700px]">
     {#if data.selectedStory}
     <div class="px-5 text-white font-mono ">
         <h1 class="text-3xl py-4">{data.selectedStory.StoryTitle}</h1>
-        <p>{data.selectedStory.StoryText}</p>
+        <p class ="text-slate-500">{data.selectedStory.StoryText}</p>
         </div>
     {:else}
         <p>Loading...</p>
     {/if}
+</div>
 </main>
