19cb3db1fba6243205741278a6a24c25cbbddcea
KoloWn <kolown@gmail.com>
2024-11-23 22:09:59 +0800

    Refactor GhostWriter story display to split text into paragraphs for improved readability

--- src/routes/text/ghostwriter/[id]/+page.svelte
+++ src/routes/text/ghostwriter/[id]/+page.svelte
@@ -2,17 +2,22 @@
     import type { PageData } from './$types';
 
     export let data: PageData;
+
+    let storyText = data.selectedStory?.StoryText || '';
+    let paragraphs = storyText.split('\n\n');
 </script>
 
 <main>
     <div class="mx-auto max-w-[700px]">
-    {#if data.selectedStory}
-    <div class="px-5 text-white font-mono ">
-        <h1 class="text-3xl py-4">{data.selectedStory.StoryTitle}</h1>
-        <p class ="text-slate-500">{data.selectedStory.StoryText}</p>
-        </div>
-    {:else}
-        <p>Loading...</p>
-    {/if}
-</div>
+        {#if data.selectedStory}
+            <div class="px-5 text-white font-mono">
+                <h1 class="text-3xl py-4">{data.selectedStory.StoryTitle}</h1>
+                {#each paragraphs as paragraph}
+                    <p class="text-slate-500 mb-4">{paragraph}</p>
+                {/each}
+            </div>
+        {:else}
+            <p>Loading...</p>
+        {/if}
+    </div>
 </main>
